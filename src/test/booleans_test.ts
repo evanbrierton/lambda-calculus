b2bd783ec0926d58fc31b2250a38bf64f4698838
Nikita Skobelevs <nikita.skobelevs@gmail.com>
2023-03-13 00:02:53 +0000

    Update tests to use new assertions

--- src/test/booleans_test.ts
+++ src/test/booleans_test.ts
@@ -1,6 +1,6 @@
 import {
-  assert,
-  assertFalse,
+  assert as rawAssert,
+  assertFalse as rawAssertFalse,
 } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 import {
   And,
@@ -13,48 +13,48 @@ import {
   ToJSBool,
 } from "../lib/booleans.ts";
 import { I, M } from "../lib/combinators.ts";
-import { assertBoolean } from "./asserts.ts";
+import { assert, assertFalse } from "./asserts.ts";
 
 Deno.test(function trueTest() {
-  assert(T(I)(M) === I);
+  rawAssert(T(I)(M) === I);
 });
 
 Deno.test(function falseTest() {
-  assert(F(I)(M) === M);
+  rawAssert(F(I)(M) === M);
 });
 
 Deno.test(function toJSBoolTest() {
-  assert(ToJSBool(T));
-  assertFalse(ToJSBool(F));
+  rawAssert(ToJSBool(T));
+  rawAssertFalse(ToJSBool(F));
 });
 
 Deno.test(function fromJSBoolTest() {
-  assertBoolean(FromJSBool(true), true);
-  assertBoolean(FromJSBool(false), false);
+  assert(FromJSBool(true));
+  assertFalse(FromJSBool(false));
 });
 
 Deno.test(function andTest() {
-  assertBoolean(And(F)(F), false);
-  assertBoolean(And(F)(T), false);
-  assertBoolean(And(T)(F), false);
-  assertBoolean(And(T)(T), true);
+  assertFalse(And(F)(F));
+  assertFalse(And(F)(T));
+  assertFalse(And(T)(F));
+  assert(And(T)(T));
 });
 
 Deno.test(function notTest() {
-  assertBoolean(Not(F), true);
-  assertBoolean(Not(T), false);
+  assert(Not(F));
+  assertFalse(Not(T));
 });
 
 Deno.test(function orTest() {
-  assertBoolean(Or(F)(F), false);
-  assertBoolean(Or(F)(T), true);
-  assertBoolean(Or(T)(F), true);
-  assertBoolean(Or(T)(T), true);
+  assertFalse(Or(F)(F));
+  assert(Or(F)(T));
+  assert(Or(T)(F));
+  assert(Or(T)(T));
 });
 
 Deno.test(function beqTest() {
-  assertBoolean(Beq(F)(F), true);
-  assertBoolean(Beq(F)(T), false);
-  assertBoolean(Beq(T)(F), false);
-  assertBoolean(Beq(T)(T), true);
+  assert(Beq(F)(F));
+  assertFalse(Beq(F)(T));
+  assertFalse(Beq(T)(F));
+  assert(Beq(T)(T));
 });
--- src/test/numerals_test.ts
+++ src/test/numerals_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
+import { assertEquals as rawAssertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 import { Eq } from "../lib/arithmetic.ts";
 import {
   FromJSNumber,
@@ -10,22 +10,22 @@ import {
   N5,
   ToJSNumber,
 } from "../lib/numerals.ts";
-import { assertBoolean } from "./asserts.ts";
+import { assert } from "./asserts.ts";
 
 Deno.test(function fromJSNumberTest() {
-  assertBoolean(Eq(FromJSNumber(0))(N0), true);
-  assertBoolean(Eq(FromJSNumber(1))(N1), true);
-  assertBoolean(Eq(FromJSNumber(2))(N2), true);
-  assertBoolean(Eq(FromJSNumber(3))(N3), true);
-  assertBoolean(Eq(FromJSNumber(4))(N4), true);
-  assertBoolean(Eq(FromJSNumber(5))(N5), true);
+  assert(Eq(FromJSNumber(0))(N0));
+  assert(Eq(FromJSNumber(1))(N1));
+  assert(Eq(FromJSNumber(2))(N2));
+  assert(Eq(FromJSNumber(3))(N3));
+  assert(Eq(FromJSNumber(4))(N4));
+  assert(Eq(FromJSNumber(5))(N5));
 });
 
 Deno.test(function toJsNumberTest() {
-  assertEquals(ToJSNumber(N0), 0);
-  assertEquals(ToJSNumber(N1), 1);
-  assertEquals(ToJSNumber(N2), 2);
-  assertEquals(ToJSNumber(N3), 3);
-  assertEquals(ToJSNumber(N4), 4);
-  assertEquals(ToJSNumber(N5), 5);
+  rawAssertEquals(ToJSNumber(N0), 0);
+  rawAssertEquals(ToJSNumber(N1), 1);
+  rawAssertEquals(ToJSNumber(N2), 2);
+  rawAssertEquals(ToJSNumber(N3), 3);
+  rawAssertEquals(ToJSNumber(N4), 4);
+  rawAssertEquals(ToJSNumber(N5), 5);
 });
